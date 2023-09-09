d1d5ad182bf94423a8b12a5ba58fcd4f3ac03f48
PurpleHead <michael.poik2003@gmail.com>
2023-09-09 10:54:47 +0200

    Removed web-dev-server

--- spotitube-client/web-dev-server.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-    open: true,
-    watch: true,
-    port: 8080,
-    appIndex: './static/index.html',
-    nodeResolve: {
-      exportConditions: ['development'],
-    },
-  };
